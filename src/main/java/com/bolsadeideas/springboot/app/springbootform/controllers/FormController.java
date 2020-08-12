6a9e665356a37afbc422d553ae4d250ba5aa8ebc
francisco <fharavena@gmail.com>
2020-08-11 23:35:05 -0400

    tercer commit

--- src/main/java/com/bolsadeideas/springboot/app/springbootform/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootform/controllers/FormController.java
@@ -1,5 +1,8 @@
 package com.bolsadeideas.springboot.app.springbootform.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.validation.Valid;
 
 import com.bolsadeideas.springboot.app.springbootform.domain.Usuario;
@@ -24,11 +27,17 @@ public class FormController {
         model.addAttribute("titulo", "Resultado Form");
 
         if (result.hasErrors()) {
+            Map<String,String> errores = new HashMap<>();
+            result.getFieldErrors().forEach(err -> {
+                errores.put(err.getField(),"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()))
+            });
+            model.addAttribute("error", errores);
             return "form";
+            
         }
 
-        model.addAttribute("usuario", usuario);
+    model.addAttribute("usuario",usuario);
 
-        return "resultado";
-    }
+    return"resultado";
+}
 }
