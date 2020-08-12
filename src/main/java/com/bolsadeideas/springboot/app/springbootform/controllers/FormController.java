ff993f61d04eabdc5099def437ed22a3e7dfa34f
Francisco Aravena <fharavena@gmail.com>
2020-08-12 00:10:40 -0400

    cuarto

--- src/main/java/com/bolsadeideas/springboot/app/springbootform/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootform/controllers/FormController.java
@@ -1,4 +1,4 @@
-package com.bolsadeideas.springboot.app.springbootform.controllers;
+package main.java.com.bolsadeideas.springboot.app.springbootform.controllers;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,9 +15,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class FormController {
-
+ 
     @GetMapping("/form")
     public String form(Model model) {
+        Usuario usuario = new Usuario();
+        model.addAttribute("usuario", usuario);
         model.addAttribute("titulo", "Formulario usuarios");
         return "form";
     }
@@ -25,18 +27,18 @@ public class FormController {
     @PostMapping("/form")
     public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
         model.addAttribute("titulo", "Resultado Form");
-
+        
         if (result.hasErrors()) {
             Map<String,String> errores = new HashMap<>();
             result.getFieldErrors().forEach(err -> {
-                errores.put(err.getField(),"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()))
+                errores.put(err.getField(),"El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
             });
             model.addAttribute("error", errores);
             return "form";
             
         }
-
-    model.addAttribute("usuario",usuario);
+        
+        model.addAttribute("usuario",usuario);
 
     return"resultado";
 }
